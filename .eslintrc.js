7480b2275c253bfb3459c5012ad354442a86b53d
rayhanyl <rayhanyuda13@gmail.com>
2022-06-11 00:27:12 +0700

    Push code

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
